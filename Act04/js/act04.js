d9f2424f3ab89437e4b410e856c9f48278e47cc4
joujoeel <60615759+joujoeel@users.noreply.github.com>
2020-03-29 23:50:40 +0200

    Add files via upload

---
+++ Act04/js/act04.js
@@ -0,0 +1,60 @@
+/*Tasca 1*/
+
+var primera = 10;
+var segona = 20;
+var tercera = 30;
+var quarta = 40;
+
+if (quarta >= 40) {
+tasca1.innerHTML += 'Tasca 1 : La quarta es la variable més gran';
+} else {
+tasca1.innerHTML += 'Valor es fals';
+}
+/* Comparacions */
+
+if (primera < segona) {
+document.write("<br>Tasca1: La segona és mes gran que la primera</br>")
+}
+if (quarta > tercera) {
+document.write("<br>Tasca1: La quarta és més gran que la tercera</br>")
+}
+
+/*Tasca 2*/
+
+var primera = 30;
+var segona = 20;
+var tercera = 10;
+var quarta = 40;
+
+if (tercera >= 10) {
+tasca2.innerHTML += 'Tasca 2 : La tercera es la variable més petita';
+} else {
+tasca2.innerHTML += 'Valor es fals';
+}
+
+if(primera != 40){
+document.write("<br>Tasca2: La primera es diferent a la quarta</br>")
+}
+if(segona > tercera){
+document.write("<br>Tasca2: La segona es mes gran que la tercera</br>")
+}
+
+
+/* Tasca 3 */
+
+var primera = 10;
+var segona = 20;
+var tercera = 30;
+var quarta = 40;
+
+if (primera >= 10) {
+tasca3.innerHTML += 'Tasca3: No hi ha cap igual';
+}else {
+tasca3.innerHTML += 'Valor es fals';
+}
+if (segona > primera){
+document.write("<br>Tasca3: La segona és més gran que la primera</br>")
+}
+if (tercera < quarta){
+document.write("<br>Tasca 3: La quarta és més gran que la tercera</br>")
+}
