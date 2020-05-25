f0344563c6b2658011a3ebf8a83b7d4a921ef1fe
joujoeel <60615759+joujoeel@users.noreply.github.com>
2020-05-25 20:17:15 +0200

    Add files via upload

---
+++ act05/js/Act05.js
@@ -0,0 +1,39 @@
+//TASCA 1
+document.write("Tasca 1");
+var y = 9;
+
+for (var x = 0; x <= 10; x++) {
+	r=y*x;
+	document.write(y+"x"+x+"="+ r + "</br>");
+}
+
+//TASCA 2
+
+document.write("Tasca 2");
+var s = 5;
+var r1= 1;
+
+for (mult = 10; r1 <= 11; mult--) {
+		document.write(s+ " x " +mult+ " = " +s*mult+ "</br>");
+	r1++;
+	if( mult == 10){
+		document.write ("</br>");
+		}
+}
+//TASCA 3
+
+document.write("Tasca 3");
+var bucle = 1;
+var cont = 0 ;
+var cont2 = 1;
+
+document.write("<table>");
+
+while(bucle <= 100){
+	if(cont == 0){
+		document.write("<tr>");
+
+	}
+
+}
+document.write("</table>")
